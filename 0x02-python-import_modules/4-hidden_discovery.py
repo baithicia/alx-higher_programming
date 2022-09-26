2c63ca378ddd34f98648f5e0e8e621218ea35772
baithicia <baithiciaa@gmail.com>
2022-09-27 01:23:55 +0300

    print one name per line, in alpha order

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,12 +4,11 @@ if __name__ == '__main__':
     for i in dir(hidden_4):
         if "__" not in i:
             print(i)
-    my_str = input("i")
-    names = [name.lower() for name in my_str.split()]
-    names.sort()
-    print("The sorted words are:")
-    for name in names:
-        print(name, end = '')
-        
+            for i in dir(hidden_4):
+                name = [i.lower() for i in dir.split()]
+                name.sort()
+                for name in dir:
+                    print(name, end = '')
 
+   
 
