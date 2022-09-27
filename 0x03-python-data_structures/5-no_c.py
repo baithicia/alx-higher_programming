0d9acd59e89c15fb212b67890ed271dc6a112dbf
baithicia <baithiciaa@gmail.com>
2022-09-27 17:08:51 +0300

    Function that removes all characters

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    string = ''
+    for letter in my_string:
+        if letter != 'c' and letter != 'c':
+            string.append(letter)
+        return ''.join(string)
+        
