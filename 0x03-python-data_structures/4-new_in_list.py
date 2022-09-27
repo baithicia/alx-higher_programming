58954840dc42d904129349a268f65fea72c84587
baithicia <baithiciaa@gmail.com>
2022-09-27 16:00:48 +0300

    Replacing an element in a new_in_list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if isinstance(my_list, list):
-        print(my_list)
+    new_list = my_list[:]
+    if isinstance(my_list, new_list):
+        print(new_list)
     if idx < 0:
-        return my_list.copy()
-    elif idx >= len(my_list):
-        return my_list.copy()
-    else:
-        my_list.replace(idx) == element
         return my_list
+    elif idx >= len(my_list):
+        return my_list
+    else:
+        new_list(idx) == element
+        return new_list
