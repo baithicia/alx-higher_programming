bf0d7c19551ab27da4018b16eb01a3cc19c991f2
baithicia <baithiciaa@gmail.com>
2022-09-27 17:12:56 +0300

    A program that removes characters

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,5 +3,5 @@ def no_c(my_string):
     string = ''
     for letter in my_string:
         if letter != 'c' and letter != 'c':
-            string.append(letter)
+            string.append("letter")
     return ''.join(string)
