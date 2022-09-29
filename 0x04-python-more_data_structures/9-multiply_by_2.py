38cdf25835c3b1a47893788ea91c557a14117e3d
baithicia <baithiciaa@gmail.com>
2022-09-29 15:41:12 +0300

    Multiply by 2

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -4,7 +4,6 @@ from operator import ne
 
 def multiply_by_2(a_dictionary):
     new_dictionary = a_dictionary.copy()
-    for i ,j in new_dictionary.items():
-        new_dictionary[i] = j * 2
+    for key in new_dictionary.items():
+        new_dictionary[key] *= 2
     return (new_dictionary)
-    
