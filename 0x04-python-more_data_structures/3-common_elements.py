e478784a7b8fc0125c565907ba94a437c157fdbd
baithicia <baithiciaa@gmail.com>
2022-09-29 11:16:43 +0300

    Return a set of common common_elements Please enter the commit message for your changes. Lines starting

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    if len(set_1.intersection(set_2)) > 0:
-        return(set_1.intersection(set_2))
-    else:
-        return("no common_elements")
-        
+    result =(i for i in set_1 if i in set_2)
+    return result
