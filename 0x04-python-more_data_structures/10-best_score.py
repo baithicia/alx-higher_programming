f8c553738a7594afe71ff1f946af4fcafb62319f
baithicia <baithiciaa@gmail.com>
2022-09-29 15:56:59 +0300

    Best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary:
-        return (max(a_dictionary))
+        for key, value in a_dictionary.items():
+            if value is max(a_dictionary.values()):
+                return key
     else:
         return None
-        
