ef75a432f826877fcf0ac508f7905510ce336af0
baithicia <baithiciaa@gmail.com>
2022-10-25 05:56:17 +0300

    square1

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-""" Import statment """
+"""Square #1"""
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """ Square class """
+    """representation of a square"""
+
     def __init__(self, size):
-        super().integer_validator("size", size)
-        self.__size = size
+        """instantiation of the square"""
+        self.integer_validator("size", size)
         super().__init__(size, size)
+        self.__size = size
