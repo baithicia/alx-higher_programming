3ea25666616458a36d7c12693b7687de5683fe28
baithicia <baithiciaa@gmail.com>
2022-10-27 06:29:19 +0300

    Pascal's triangle

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,30 +1,30 @@
 #!/usr/bin/python3
-"""
-Class Module
-"""
+'''
+Write a class Student that defines a student
+'''
 
 
 class Student:
-    """
+    '''
     Student class
-    """
+    '''
 
     def __init__(self, first_name, last_name, age):
-        """initialize method
-        """
+        '''
+        Constructor
+        '''
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """retrieves a dictionary representation of student instance
-        args:
-            attrs: attributes
-        return:
-            dictionary
-        """
-        if not attrs:
+        '''
+        Retrieves dict
+        '''
+        if attrs is None:
             return self.__dict__
-
-        return ({key: value for key, value in self.__dict__.items()
-                 if key in attrs})
+        my_dict = {}
+        for items in attrs:
+            if hasattr(self, items):
+                my_dict[items] = getattr(self, items)
+        return my_dict
