c49f3cf6d4a8d6badf0a87cf9eaa3e85b2f31222
baithicia <baithiciaa@gmail.com>
2022-11-13 22:49:17 +0300

    printing a square

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,38 +1,38 @@
 #!/usr/bin/python3
 """ Printing a square"""
 
+
 class Square:
-    """defines a square  with private attributes"""
-    def __init__(self,size=0):  
-        """initializes private attributes"""
-        
+    """Private instance attribute: size
+    Instantiation with area method prints squares """
+
+    def __init__(self, size=0):
+        """Initializes attribute size """
+        self.__size = size
+
     def area(self):
-        """method for calculating area of a square object"""
-        return  (self.__size * self.__size)
+        """Calculate area of square"""
+        return (self.__size * self.__size)
 
     @property
     def size(self):
-        """getter method"""
-        return self.__size 
-    
+        """Setter for square"""
+        return self.__size
+
     @size.setter
     def size(self, value):
-        """setter method"""
-        if type(value) != int:
-            raise TypeError ("size must be an integer")
+        """Initializes attribute size """
+        if (type(value) is not int):
+            raise TypeError("size must be an integer")
         elif value < 0:
-            raise ValueError("size must be >=0")
+            raise ValueError("size must be >= 0")
         else:
             self.__size = value
 
     def my_print(self):
         for i in range(self.__size):
             for j in range(self.__size):
-                print("#", end = '')
-        if self.size ==0:
+                print('#', end="")
             print()
-            
-        
-
-
-
+        if self.size <= 0:
+            print()
