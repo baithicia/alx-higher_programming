bceeaffaeeae38156ff454a0874f5ace2cc8d015
baithicia <baithiciaa@gmail.com>
2022-11-13 12:51:38 +0300

    add files

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+"""Filename:  0-square"""
+class Square:
+    """defines a square"""
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""filename: 1-square"""
+
+class Square:
+    """defines a square  with private attributes"""
+    def __init__(self,size=None):
+        """initializes private attributes"""
+        self.__size = size
